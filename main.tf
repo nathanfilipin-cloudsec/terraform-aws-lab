98ee8b31a52b2484ec9500535191e3df35662bba
NathanFilipin-CloudSec <nathanfilipin20@gmail.com.br>
2026-05-23 21:29:55 -0300

    feat: creat aws vpc and subnet architecture

--- main.tf
+++ main.tf
@@ -8,17 +8,39 @@ resource "aws_s3_bucket" "teste" {
 resource "aws_vpc" "main-vpc" {
   cidr_block = "10.0.0.0/16"
 
-   tags = {
+  tags = {
     Name = "VPC"
   }
 }
 
-resource "aws_subnet" "main-subnet" {
-    vpc_id = aws_vpc.main-vpc
-    cidr_block = "10.0.0.0/16"
+resource "aws_subnet" "subnet1" {
+  vpc_id     = aws_vpc.main-vpc.id
+  cidr_block = "10.0.1.0/24"
 
-    tags = {
-    Name = "SUBNET"
-  } 
-  
+  tags = {
+    Name = "SUBNET1"
+  }
+
+}
+
+
+resource "aws_subnet" "subnet2" {
+  vpc_id     = aws_vpc.main-vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "SUBNET2"
+  }
+
+}
+
+
+resource "aws_subnet" "subnet3" {
+  vpc_id     = aws_vpc.main-vpc.id
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "SUBNET3"
+  }
+
 }
