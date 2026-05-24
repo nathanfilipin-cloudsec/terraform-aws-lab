83d4f45ddfa5319e5d3a89ee420d51ca29c66d75
NathanFilipin-CloudSec <nathanfilipin20@gmail.com.br>
2026-05-23 22:35:00 -0300

    feat: add internet gatteway and public route table

--- main.tf
+++ main.tf
@@ -13,6 +13,14 @@ resource "aws_vpc" "main-vpc" {
   }
 }
 
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  tags = {
+    Name= "main_igw"
+  }
+}
+
 resource "aws_subnet" "subnet1" {
   vpc_id     = aws_vpc.main-vpc.id
   cidr_block = "10.0.1.0/24"
@@ -43,4 +51,22 @@ resource "aws_subnet" "subnet3" {
     Name = "SUBNET3"
   }
 
+}
+
+resource "aws_route_table" "route_table" {
+vpc_id = aws_vpc.main-vpc.id
+
+route {
+  cidr_block= "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.main_igw.id
+}
+tags = {
+  Name= "route_table"
+}
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.route_table.id
+  
 }
